0988a7384ca494dc372cab97efa160cf9cbf31a7
bloomy-teach <brandonrbloomfield@gmail.com>
2026-04-06 11:31:26 -0400

    a
    to fix

--- setup.bat
+++ setup.bat
@@ -1,4 +1,3 @@
-
 @echo off
 
 REM Create virtual environment
@@ -7,7 +6,17 @@ python -m venv venv
 REM Activate virtual environment
 call venv\Scripts\activate.bat
 
+REM Upgrade pip, setuptools, and wheel
+python -m pip install --upgrade pip setuptools wheel
+
+REM Clear pip cache
+pip cache purge
+
+REM Install numpy with precompiled binaries
+pip install numpy --only-binary=:all:
+
 REM Install required packages
-pip install -r requirements.txt
+pip install --use-deprecated=legacy-resolver -r "%~dp0requirements.txt"
+
 REM Run the main.py script
-python main.py
+python "%~dp0main.py"
